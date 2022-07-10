a3d095992b2706e8e3679d1c12f4e1af24174198
Edgar Mendez <edgar.ym26@gmail.com>
2022-07-10 19:53:37 -0400

    server js finished

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require ("express");
 const PORT = process.env.PORT || 3001;
 
-
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require("./routes"));
 
 
 
