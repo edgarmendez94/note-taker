3d623d6fc25d773d7b648efbb89e35f86fdd89bf
Edgar Mendez <edgar.ym26@gmail.com>
2022-07-10 20:45:33 -0400

    notes routes almost done i think

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -1,5 +1,17 @@
 const router = require("express").Router();
 const path = require("path");
 
+router.get("/", (req,res) => 
+    res.sendFile(path.join())
+);
+
+
+router.get("/notes", (req,res) => 
+    res.sendFile(path.join())
+);
+
+
+
+
 
 module.exports = router;
