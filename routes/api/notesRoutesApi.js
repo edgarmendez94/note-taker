bbae0e0b61cf01dcd72b3e08224982d9ddefc9a6
Edgar Mendez <edgar.ym26@gmail.com>
2022-07-10 21:18:17 -0400

    notes api requires all things , has some routes that will be adjusted

--- routes/api/notesRoutesApi.js
+++ routes/api/notesRoutesApi.js
@@ -1,6 +1,21 @@
 const router = require("express").Router();
 const path = require("path");
 const fs = require("fs");
+const {uuidv4} = require("uuid");
+
+router.get("/notes", (req,res) => 
+    res.sendFile(path.join(__dirname,""))
+);
+
+router.get("/notes/:id", (req,res) => 
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+);
+
+
+router.post("/notes", (req,res) => 
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+);
+
 
 
 
