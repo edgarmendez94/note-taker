227178f9c2186420c16d442cf26baaccf1930517
Edgar Mendez <edgar.ym26@gmail.com>
2022-07-10 21:05:07 -0400

    non api routes done

--- routes/api/notesRoutesApi.js
+++ routes/api/notesRoutesApi.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const path = require("path");
-
+const fs = require("fs");
 
 
 
--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const path = require("path");
 
 router.get("/", (req,res) => 
-    res.sendFile(path.join())
+    res.sendFile(path.join(__dirname,"../public/index.html"))
 );
 
 
 router.get("/notes", (req,res) => 
-    res.sendFile(path.join())
+    res.sendFile(path.join(__dirname,"../public/notes.html"))
 );
 
 
